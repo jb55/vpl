9a70e64026061eae3b1d38631aca1775172f290f
William Casarin <bill@casarin.me>
2016-02-27 21:13:02 -0800

    Fix whitespace

--- ide/render.c
+++ ide/render.c
@@ -10,62 +10,62 @@ vpl_draw_pins(struct vpl_ctx *vpl, struct vpl_node * node);
 
 void
 vpl_draw_node(struct vpl_ctx *vpl, struct vpl_node *node) {
-	NVGpaint shadowPaint;
-	NVGpaint headerPaint;
+  NVGpaint shadowPaint;
+  NVGpaint headerPaint;
 
-	float cornerRadius = node->roundness;
-	NVGcontext *vg = vpl->nvg;
+  float cornerRadius = node->roundness;
+  NVGcontext *vg = vpl->nvg;
 
-	float x = node->x;
-	float y = node->y;
-	float w = node->w;
-	float h = node->h;
+  float x = node->x;
+  float y = node->y;
+  float w = node->w;
+  float h = node->h;
 
-	char *title = node->title;
+  char *title = node->title;
 
   nvgSave(vg);
 
-	// Window
-	nvgBeginPath(vg);
-	nvgRoundedRect(vg, x,y, w,h, cornerRadius);
-	nvgFillColor(vg, nvgRGBA(28,30,34,192));
-//	nvgFillColor(vg, nvgRGBA(0,0,0,128));
-	nvgFill(vg);
+  // Window
+  nvgBeginPath(vg);
+  nvgRoundedRect(vg, x,y, w,h, cornerRadius);
+  nvgFillColor(vg, nvgRGBA(28,30,34,192));
+//  nvgFillColor(vg, nvgRGBA(0,0,0,128));
+  nvgFill(vg);
 
-	// Drop shadow
-	shadowPaint = nvgBoxGradient(vg, x,y+2, w,h, cornerRadius*2, 10,
+  // Drop shadow
+  shadowPaint = nvgBoxGradient(vg, x,y+2, w,h, cornerRadius*2, 10,
                                nvgRGBA(0,0,0,128), nvgRGBA(0,0,0,0));
-	nvgBeginPath(vg);
-	nvgRect(vg, x-10,y-10, w+20,h+30);
-	nvgRoundedRect(vg, x,y, w,h, cornerRadius);
-	nvgPathWinding(vg, NVG_HOLE);
-	nvgFillPaint(vg, shadowPaint);
-	nvgFill(vg);
+  nvgBeginPath(vg);
+  nvgRect(vg, x-10,y-10, w+20,h+30);
+  nvgRoundedRect(vg, x,y, w,h, cornerRadius);
+  nvgPathWinding(vg, NVG_HOLE);
+  nvgFillPaint(vg, shadowPaint);
+  nvgFill(vg);
 
-	// Header
-	headerPaint = nvgLinearGradient(vg, x,y,x,y+15, nvgRGBA(255,255,255,8),
+  // Header
+  headerPaint = nvgLinearGradient(vg, x,y,x,y+15, nvgRGBA(255,255,255,8),
                                   nvgRGBA(0,0,0,16));
-	nvgBeginPath(vg);
-	nvgRoundedRect(vg, x+1,y+1, w-2,30, cornerRadius-1);
-	nvgFillPaint(vg, headerPaint);
-	nvgFill(vg);
-	nvgBeginPath(vg);
-	nvgMoveTo(vg, x+0.5f, y+0.5f+30);
-	nvgLineTo(vg, x+0.5f+w-1, y+0.5f+30);
-	nvgStrokeColor(vg, nvgRGBA(0,0,0,32));
-	nvgStroke(vg);
+  nvgBeginPath(vg);
+  nvgRoundedRect(vg, x+1,y+1, w-2,30, cornerRadius-1);
+  nvgFillPaint(vg, headerPaint);
+  nvgFill(vg);
+  nvgBeginPath(vg);
+  nvgMoveTo(vg, x+0.5f, y+0.5f+30);
+  nvgLineTo(vg, x+0.5f+w-1, y+0.5f+30);
+  nvgStrokeColor(vg, nvgRGBA(0,0,0,32));
+  nvgStroke(vg);
 
-	nvgFontSize(vg, 18.0f);
-	nvgFontFace(vg, "sans-bold");
-	nvgTextAlign(vg,NVG_ALIGN_CENTER|NVG_ALIGN_MIDDLE);
+  nvgFontSize(vg, 18.0f);
+  nvgFontFace(vg, "sans-bold");
+  nvgTextAlign(vg,NVG_ALIGN_CENTER|NVG_ALIGN_MIDDLE);
 
-	/* nvgFontBlur(vg,2); */
-	nvgFillColor(vg, nvgRGBA(0,0,0,128));
-	nvgText(vg, x+w/2,y+16+1, title, NULL);
+  /* nvgFontBlur(vg,2); */
+  nvgFillColor(vg, nvgRGBA(0,0,0,128));
+  nvgText(vg, x+w/2,y+16+1, title, NULL);
 
-	/* nvgFontBlur(vg,0); */
-	nvgFillColor(vg, nvgRGBA(220,220,220,160));
-	nvgText(vg, x+w/2,y+16, title, NULL);
+  /* nvgFontBlur(vg,0); */
+  nvgFillColor(vg, nvgRGBA(220,220,220,160));
+  nvgText(vg, x+w/2,y+16, title, NULL);
 
   nvgRestore(vg);
 
@@ -144,11 +144,11 @@ vpl_draw_pin(struct vpl_ctx *vpl,
 
   pin_get_positioning(node, pins, pin_side, pin_count, pin_ind, &x, &y);
 
-	nvgBeginPath(vg);
-	nvgRoundedRect(vg, x, y, size, size, size);
-	nvgFillColor(vg, nvgRGBAf(pin->color.r, pin->color.g, pin->color.b,
+  nvgBeginPath(vg);
+  nvgRoundedRect(vg, x, y, size, size, size);
+  nvgFillColor(vg, nvgRGBAf(pin->color.r, pin->color.g, pin->color.b,
                             pin->color.a));
-	nvgFill(vg);
+  nvgFill(vg);
 }
 
 
